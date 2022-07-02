8195ea79120418665771f009131fd0e5e2ea0fc1
Brandon Ong <9078076+brandonongzy@users.noreply.github.com>
2022-07-03 03:29:13 +0800

    added dequeue, validator and health check

--- master-decision-client/master-decision-node.py
+++ master-decision-client/master-decision-node.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 import grpc
+import requests
 
 import waitingroom_pb2
 import waitingroom_pb2_grpc
@@ -9,12 +10,36 @@ import waitingroom_pb2_grpc
 def validator():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = waitingroom_pb2_grpc.ValidatorStub(channel)
-
+        # TODO: change token request type to just a random string (base64?)
         response = stub.ValidateToken(waitingroom_pb2.ValidateTokenRequest())
         if response.success:
             print(f"Success!: {response.message}")
         else:
             print(f"Error!: {response.message}")
+
+
+def dequeue():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = waitingroom_pb2_grpc.DequeueStub(channel)
+
+        response = stub.DequeueRandomCustomer(waitingroom_pb2.DequeueCustomerRequest())
+        # response = stub.DequeueFirstCustomer(waitingroom_pb2.DequeueCustomerRequest())
+        return {
+            "ipaddr": response.ipaddr,
+            "macaddr": response.macaddr,
+            "phonenum": response.phonenum,
+            "inTime": response.inTime,
+            "outTime": response.outTime,
+        }
+
+
+def health_check():
+    # TODO: REST request to final website to check for status (availabilty and pending tokens to be validated)
+    # if there are pending tokens:
+    #   connect as grpc client to queue server to validate
+    # if there are available slots from final website:
+    #   connect as grpc client to queue server to allow for dequeuing
+    pass
 
 
 if __name__ == "__main__":
