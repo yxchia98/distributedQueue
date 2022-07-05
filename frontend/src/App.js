ddeb2c777ffbf5bab486c778595a9f83ae198ea6
justinooi <justinooi@hotmail.sg>
2022-07-05 16:21:34 +0800

    Replaced macaddr with Session ID

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ReCAPTCHA from "react-google-recaptcha";
+import React, { useRef, useState } from "react";
 
 const { WaitingRoomClient } = require("./waitingroom_grpc_web_pb.js");
-
 const {
   EnqueueCustomerRequest,
   EnqueueCustomerReply,
@@ -13,11 +14,36 @@ const {
 let client = new WaitingRoomClient("http://localhost:49334", null, null);
 
 function App() {
+  const captchaRef = useRef(null);
+  const [phoneNum, setPhoneNum] = useState(null);
+
+  let startQueue = (e) => {
+    e.preventDefault();
+    const response = captchaRef.current.getValue();
+
+    if (response.length == 0) {
+      console.log("Not checked");
+    } else {
+      const request = new EnqueueCustomerRequest();
+      request.setIpaddr("Test");
+      request.setSessionid("Test");
+      request.setPhonenum("Test");
+
+      client.enqueueCustomer(request, {}, (err, response) => {
+        if (response == null) {
+          console.log(err);
+        } else {
+          console.log(response.getStatus());
+        }
+      });
+    }
+  };
+
   let startGrpcStream = () => {
     const request = new WaitQueueRequest();
 
     request.setIpaddr("Test");
-    request.setMacaddr("Test");
+    request.setSessionid("Test");
     request.setPhonenum("Test");
 
     let stream = client.waitQueue(request);
@@ -30,28 +56,19 @@ function App() {
     });
   };
 
-  let callGrpcService = () => {
-    const request = new EnqueueCustomerRequest();
-    request.setIpaddr("Test");
-    request.setMacaddr("Test");
-    request.setPhonenum("Test");
-
-    client.enqueueCustomer(request, {}, (err, response) => {
-      if (response == null) {
-        console.log(err);
-      } else {
-        console.log(response.getStatus());
-      }
-    });
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button style={{ padding: 10 }} onClick={callGrpcService}>
-          Click for grpc request
-        </button>
+        <form onSubmit={startQueue}>
+          <label htmlFor="phoneNum">Phone Number</label>
+          <input type="text" id="phoneNum" className="input" />
+          <ReCAPTCHA
+            sitekey="6Ld-ecUgAAAAAKttkbASYR7ll4n--Q5-dNe2_ZUt"
+            ref={captchaRef}
+          />
+          <button>Submit</button>
+        </form>
         <button style={{ padding: 10 }} onClick={startGrpcStream}>
           Click for grpc stream
         </button>
