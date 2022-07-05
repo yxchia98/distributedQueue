512d89a859533a087ee3914ea39d77265c7e00bc
Brandon Ong <9078076+brandonongzy@users.noreply.github.com>
2022-07-05 17:06:10 +0800

    added final website

--- final-website/app.py
+++ final-website/app.py
@@ -1,60 +1,68 @@
-from flask import Flask, render_template, url_for, redirect,request,jsonify
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 import random
 import time
+import json
+
 app = Flask(__name__)
 
-tokenlist=[]
-@app.route('/')
+tokenlist = []
+
+
+@app.route("/")
 def home():
-    return render_template('main.html'),{"Refresh": "3; redirect"}
+    return render_template("main.html"), {"Refresh": "3; redirect"}
+
 
 # GET token and save in list
-@app.route('/gettoken')
+@app.route("/gettoken")
 def gettoken():
     # if key doesn't exist, returns None
-    token = request.args.get('token')
+    token = request.args.get("token")
     tokenlist.append(token)
-    return '''
-              <h1>The token is: {}</h1>'''.format(token)
+    return """
+              <h1>The token is: {}</h1>""".format(
+        token
+    )
 
-#send token for validation
-@app.route('/sendtoken', methods=['GET'])
+
+# send token for validation
+@app.route("/sendtoken", methods=["GET"])
 def starting_url():
     return jsonify(tokenlist)
 
-#validate token 
-@app.route('/validatetoken', methods=['POST'])
+
+# validate token
+@app.route("/validatetoken", methods=["POST"])
 def validate():
-    request_data = request.get_json()
-    token = request_data['token']
+    request_data = json.loads(request.get_json())
+    print(request_data)
+    token = request_data["token"]
     global result
-    result = request_data['result']
-    for elem in list(tokenlist):
-        if tokenlist[elem] == token:
+    result = request_data["result"]
+    for elem in tokenlist:
+        print(elem)
+        if elem == token:
             tokenlist.remove(elem)
-        else:
-            return '''
-              <h1>error</h1>'''
-   
+    return jsonify("no tokens found")
 
 
-#deicde which site to redirect to
-@app.route('/redirect')
+# deicde which site to redirect to
+@app.route("/redirect")
 def options():
     if result == "True":
         return redirect("/option1")
     else:
         return redirect("/option2")
-   
-@app.route('/option1')
+
+
+@app.route("/option1")
 def option1():
-    return render_template('success.html')
+    return render_template("success.html")
 
-@app.route('/option2')
+
+@app.route("/option2")
 def option2():
-    return render_template('index.html')
-
-
+    return render_template("index.html")
 
 
 app.run(debug=True)
